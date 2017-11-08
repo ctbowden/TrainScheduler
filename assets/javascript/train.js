dff6fe3a8ae9e83bc626cc62c10314111b50b718
unknown <charles.bowden@gmail.com>
2017-11-08 16:25:29 -0500

    HTML updated

---
+++ assets/javascript/train.js
@@ -0,0 +1,14 @@
+
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAXFIGXFboh8Jvto5SMkXhTAcrhDkp5PL4",
+    authDomain: "trainscheduler-933df.firebaseapp.com",
+    databaseURL: "https://trainscheduler-933df.firebaseio.com",
+    projectId: "trainscheduler-933df",
+    storageBucket: "",
+    messagingSenderId: "882042511741"
+  };
+  firebase.initializeApp(config);
+</script>
